f9e9970ba2893ce09929e2277ef6d744e2085148
JT Archie <jtarchie@gmail.com>
2011-10-23 20:26:38 -0400

    init rails for sprokets

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Touch::Application.config.secret_token = '68567ba90ce0875c877aadca74612b803b83e0875cd6de5d24747cd08b4f7672c5886f7a3bab56be85f22396df0fdb9d48641d14c9b9588d9bcad54217f2ed8e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Touch::Application.config.session_store :cookie_store, key: '_touch_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Touch::Application.config.session_store :active_record_store
